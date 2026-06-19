80fde2351f17e528f47dc092a4b079b86db528f7
ImJaePang <narushel@naver.com>
2026-06-19 12:49:36 +0900

    7. JUnit5로 테스트하기

--- section01_cafeKiosk/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ section01_cafeKiosk/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -2,17 +2,57 @@ package sample.cafekiosk.unit;
 
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverages.Americano;
+import sample.cafekiosk.unit.beverages.Latte;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CafeKioskTest {
+
+    @Test
+    void add_manual_test() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add((new Americano()));
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
 
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add((new Americano()));
 
-        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
-        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add((new Americano()));
+
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+
+        cafeKiosk.remove(cafeKiosk.getBeverages().get(0));
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+
+
+    }
+
+    @Test
+    void clear() {
+
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add((new Americano()));
+        cafeKiosk.add((new Latte()));
+
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(2);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 }
