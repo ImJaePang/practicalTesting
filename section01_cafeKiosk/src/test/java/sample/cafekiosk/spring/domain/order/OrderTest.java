fe4a2aea6202caae74f7e17812222d838f83520c
ImJaePang <narushel@naver.com>
2026-06-27 12:44:40 +0900

    Business Layer 테스트 (1) - 일부 수정

---
+++ section01_cafeKiosk/src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -0,0 +1,21 @@
+package sample.cafekiosk.spring.domain.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @DisplayName("주문 생성 시 상품 리스트에서 주문의 총 금액을 계산한다.")
+    @Test
+    void caclulateTotalPrice() {
+        // given
+//        List.of(Product)
+
+        // when
+
+        // then
+
+    }
+}
