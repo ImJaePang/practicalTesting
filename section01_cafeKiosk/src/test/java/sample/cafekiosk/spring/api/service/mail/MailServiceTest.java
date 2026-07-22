4d1da0a44e1b3f11b6b20248f0482c1145ee6d48
ImJaePang <narushel@naver.com>
2026-07-22 12:25:29 +0900

    BDDMockito

--- section01_cafeKiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ section01_cafeKiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,10 +3,7 @@ package sample.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
@@ -40,15 +37,19 @@ class MailServiceTest {
 
 //        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
 
-        doReturn(true)
-                .when(mailSendClient)
-                .sendEmail(anyString(), anyString(), anyString(), anyString());
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(), anyString(), anyString(), anyString());
 
 //        Mockito.when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
 //                .thenReturn(true);
 //        Mockito.when(mailSendHistoryRepository.save(any(MailSendHistory.class)))
 //                .thenReturn(null);
 
+        BDDMockito.given(mailSendClient
+                        .sendEmail(anyString(), anyString(), anyString(), anyString())
+                )
+                .willReturn(true);
 
         //when
         boolean result = mailService.sendMail("","","","");
